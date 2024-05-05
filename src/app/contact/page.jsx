c2f9e6dce9d4d0de1177b598f99fa8ca74ea31e7
Maitulya Vaghela <vaghelamaitulya85@gmail.com>
2024-05-05 15:14:44 +0530

    Send Email

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,36 @@
 "use client"
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { motion } from "framer-motion"
+import emailjs from '@emailjs/browser';
+
 const ContactPage = () => {
+  const [success, setSuccess] = useState(false)
+  const [error, setError] = useState(false)
+  const text = "Say Hello"
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false)
+    setSuccess(false)
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        form.current,
+        {publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY})
+      .then(
+        () => {
+          setSuccess(true)
+          form.current.reset()
+        },
+        (error) => {
+          setError(true)
+          console.log(error.message)
+        },
+      );
+  };
   return (
     <motion.div
       className="h-full"
@@ -9,7 +38,49 @@ const ContactPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-        contact page
+      <div className='h-full flex flex-col lg:flex-row  px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+        {/* TEXT CONTAINER  */}
+        <div className='h-1/2 lg:w-1/2 lg:h-full flex items-center justify-center text-6xl'>
+          {text.split("").map((letter, index) => (
+            <motion.span
+              key={index}
+              initial={{ opacity: 1 }}
+              animate={{ opacity: 0 }}
+              transition={{
+                duration: 3,
+                repeat: Infinity,
+                delay: index * 0.1
+              }}
+            >
+              {letter}
+            </motion.span>
+
+          ))}
+          😊
+        </div>
+        {/* FORM CONTAINER  */}
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className='h-1/2 lg:w-1/2 lg:h-full bg-red-50 flex flex-col gap-8 p-24 rounded-xl text-xl'
+        >
+          <span name="to_name">Dear Maitulya,</span>
+          <textarea
+            name='user_message'
+            rows={6}
+            className='bg-transparent border-b-2 border-b-black outline-none resize-none' />
+          <span>My mail address is:</span>
+          <input
+            name='user_email'
+            type='text'
+            className='bg-transparent border-b-2 border-b-black outline-none ' />
+          <span>My Regards</span>
+          <button className='bg-purple-200 p-4 font-semibold rounded text-gray-600'>Submit</button>
+          {success && <span className='text-green-600 font-semibold'>Your message has been send successfully!</span>}
+          {error && <span className='text-red-600 font-semibold'>Something went wrong!</span>}
+        </form>
+
+      </div>
     </motion.div>
   )
 }
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -50,24 +50,60 @@ const PortfolioPage = () => {
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
-      
+
     >
       <div className='h-[600vh] relative' ref={ref}>
-        <div className='w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-center text-8xl'>
-          My Works
+        <div className=''>
+          <div className=' w-screen h-[calc(100vh-6rem)] flex flex-col gap-40 items-center justify-center text-center text-5xl md:text-6xl lg:text-7xl xl:text-8xl'>
+            My Works
+            <motion.svg
+                initial={{ opacity: 0.2, y: 0 }}
+                animate={{ opacity: 1, y: "10px" }}
+                transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                width={50}
+                height={50}
+              >
+                <path
+                  d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                  stroke="#000000"
+                  strokeWidth="1"
+                ></path>
+                <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+                <path
+                  d="M15 11L12 14L9 11"
+                  stroke="#000000"
+                  strokeWidth="1"
+                ></path>
+              </motion.svg>
+          </div>
+    
         </div>
         <div className='sticky top-0 flex  h-screen  items-center gap-4 overflow-hidden no-scrollbar'>
-          <motion.div style={{x}} className='flex'>
-          <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300 "/>
+          <motion.div style={{ x }} className='flex'>
+            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300 " />
             {items.map((item) => (
-            <div key={item.id} className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}>
+              <div key={item.id} className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}>
                 <div className='flex flex-col gap-8 text-white'>
-                  <h1 className=''>{item.title}</h1>
-                  <div className='relative'>
+                  <h1 className='text-xl font-bold md:text-2xl lg:text-4xl xl:text-8xl'>{item.title}</h1>
+                  <div className='relative w-80 h-56  lg:w-[600px] lg:h-[350px] xl:w-[700px] xl:h-[420px]'>
                     <Image src={item.img} alt={item.title} fill />
                   </div>
-                  <p>{item.desc}</p>
-                  <Link href={item.link}><button>Live Demo</button></Link>
+                  <p className='w-80 lg:w-[500px] xl:w-[600px] lg:text-lg'>{item.desc}</p>
+                  <div className='flex gap-4'>
+                    <Link href={item.link}>
+                      <button className='p-2 text-sm md:p-4 md:text-base lg:p-5 lg:text-lg bg-white text-gray-600 rounded font-semibold my-2'>
+                        Live Demo
+                      </button>
+                    </Link>
+                    <Link href={item.link}>
+                      <button className='p-2 text-sm md:p-4 md:text-base lg:p-5 lg:text-lg bg-white text-gray-600 rounded  font-semibold my-2'>
+                        Source Code
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </div>
             ))}
@@ -95,12 +131,18 @@ const PortfolioPage = () => {
               </textPath>
             </text>
           </motion.svg>
-          <Link
+          <motion.a
             href="/contact"
-            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center transition duration-300 ease-in-out"
+            whileHover={{
+              backgroundImage: 'linear-gradient(to right, #805ad5, #f56565)',
+              transition: { duration: 0.3 }
+            }}
+            initial={{ backgroundImage: 'linear-gradient(to right, #000000, #000000)' }}
           >
             Hire Me
-          </Link>
+          </motion.a>
+
         </div>
       </div>
     </motion.div>
